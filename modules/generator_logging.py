8da9ddda722a8ee294a8f96cadc03557851dbb24
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2022-10-24 17:27:09 +0200

    Added divider and changed info color

--- modules/generator_logging.py
+++ modules/generator_logging.py
@@ -4,7 +4,7 @@ class Color(Enum):
     NOTIFY = '\033[93m'
     SUCCESS = '\033[92m'
     FAIL = '\033[91m'
-    INFO = '\033[94m'
+    INFO = '\033[36m'
     END = '\033[0m'
 
 def log(message:str, color:Color, tabs:int = 1, end:str = '\n'):
@@ -21,3 +21,6 @@ def log_notify(message:str, tabs:int = 1, end:str = '\n'):
 
 def log_info(message:str, tabs:int = 1, end:str = '\n'):
     log(message, Color.INFO, tabs, end)
+
+def log_divider(tabs:int = 1):
+    log('------------------------------------------------------------------------', Color.INFO, tabs)
